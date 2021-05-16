407216be80db4d58a60d554ce260e25969469acc
STDSuperman <2750556766@qq.com>
2021-05-16 13:21:40 +0800

    :sparkles: feat: 添加手写bind与测试

--- src/js/call-apply-bind.ts
+++ src/js/call-apply-bind.ts
@@ -9,9 +9,8 @@ interface ThisArg {
 interface Function {
     myCall: <T, A extends any[], R>(this: (this: T, ...args: A) => R, thisArg: T, ...args: A) => R;
     myApply: <T, A extends any[], R>(this: (this: T, ...args: A) => R, thisArg: T, args?: A) => R;
+    myBind: (this: Function, thisArg: any, ...args: any[]) => any;
 }
-
-Object.prototype.toString.call
 
 Function.prototype.myCall = function(this, thisArg: any, ...args) {
     const fn = Symbol('fn')
@@ -27,4 +26,25 @@ Function.prototype.myApply = function (this, thisArg: any, args) {
     const result = thisArg[fn](args);
     delete thisArg.fn;
     return result;
+}
+
+interface TempF {
+    new(): any
+}
+
+Function.prototype.myBind = function (this, thisArg: any, ...args) {
+    if (typeof this !== "function") {
+        throw new Error("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+    const self = this;
+    function fn(this: Function, ...childArgs: any[]) {
+        return self.apply(this instanceof fn ? this : thisArg, args.concat(childArgs));
+    }
+
+    if (self.prototype) {
+        const f = function () {} as any as TempF;
+        f.prototype = self.prototype;
+        fn.prototype = new f();
+    }
+    return fn;
 }
--- src/js/test/call-apply-bind.test.ts
+++ src/js/test/call-apply-bind.test.ts
@@ -10,4 +10,26 @@ describe('测试call-apply-bind', () => {
         expect(Object.prototype.toString.myApply([])).toEqual('[object Array]');
         expect(Array.prototype.concat.myApply([], [1, 2, 3])).toEqual([1, 2, 3])
     })
+
+    test('bind', () => {
+        function getAFn (...args) {
+            const myArgs = args.concat(this.a || []);
+            this.args = myArgs;
+            return this.a + ',' + args.toString();
+        }
+        const obj = {
+            a: 1,
+            getA: getAFn
+        }
+
+        const obj2 = {a: 2}
+
+        const getA = obj.getA.myBind(obj2, 1, 2);
+        expect(getA()).toEqual('2,1,2');
+        expect(getA(3, 4)).toEqual('2,1,2,3,4')
+        // 判断传参是否合并至初始参数
+        expect((new getA(3)).args).toEqual([1, 2, 3]);
+        // 测试是否为继承关系
+        expect(new getA instanceof obj.getA).toBeTruthy()
+    })
 })
