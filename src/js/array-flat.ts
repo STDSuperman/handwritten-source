34384c23c7974063727d8577204ed395af82ef43
STDSuperman <2750556766@qq.com>
2021-05-19 22:25:02 +0800

    :beers: feat: 添加数组扁平化循环方式

--- src/js/array-flat.ts
+++ src/js/array-flat.ts
@@ -2,8 +2,24 @@
  * 数组扁平化
  */
 
+// 递归 + reduce
 export function flatByReduce(arr: any[]): any[] {
     return arr.reduce((pre, cur) => {
         return pre.concat(Array.isArray(cur) ? flatByReduce(cur) : cur)
     }, []);
+}
+
+// 循环加额外栈
+export function flatByCycle(arr: any[]): any[] {
+    const stack = arr.slice();
+    const result = [];
+    while(stack.length) {
+        const item = stack.shift();
+        if (Array.isArray(item)) {
+            stack.unshift(...item);
+        } else {
+            result.push(item);
+        }
+    }
+    return result;
 }
--- src/js/test/array-flat.test.ts
+++ src/js/test/array-flat.test.ts
@@ -1,7 +1,11 @@
-import { flatByReduce } from '../array-flat';
+import { flatByReduce, flatByCycle } from '../array-flat';
 
 describe('数组扁平化', () => {
     test('flatByReduce', () => {
         expect(flatByReduce([1, [2, [3, [4, [5, [6, [7]]]]]]])).toEqual([1, 2, 3, 4, 5, 6, 7])
+    })
+
+    test('flatByCycle', () => {
+        expect(flatByCycle([1, [2, [3, [4, [5, [6, [7]]]]]]])).toEqual([1, 2, 3, 4, 5, 6, 7])
     })
 })
