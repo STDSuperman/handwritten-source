642c98f10bc3ab4852844af4cf40353d29f294ba
STDSuperman <2750556766@qq.com>
2021-06-26 16:51:08 +0800

    :sparkles: feat: 添加二分查找

--- src/leetcode/ts-implement/lc.ts
+++ src/leetcode/ts-implement/lc.ts
@@ -71,4 +71,25 @@ function threeSum(nums: number[]): number[][] {
     }
     return result;
 };
-console.log(threeSum([-1,0,1,2,-1,-4]));
+// console.log(threeSum([-1,0,1,2,-1,-4]));
+
+
+// 二分查找
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left <= right) {
+        const mid = ~~(left + (right - left) / 2);
+        if (target === nums[mid]) {
+            return mid;
+        } else if (target > nums[mid]) {
+            left = mid + 1;
+        } else if (target < nums[mid]) {
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
+
+console.log(search([-1,0,3,5,9,12], 9));
