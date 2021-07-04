dae5d53820f78204b8f7cb990ffc457b8ff5289f
STDSuperman <2750556766@qq.com>
2021-07-04 14:28:25 +0800

    :sparkles: feat：添加力扣编辑距离动规解法

--- src/leetcode/ts-implement/dp.ts
+++ src/leetcode/ts-implement/dp.ts
@@ -40,4 +40,46 @@ function coinChange(coins: number[], amount: number): number {
     return memo[amount] !== amount + 1 ? memo[amount] : -1;
 };
 
-console.log(coinChange([1,2,5], 11))
+// console.log(coinChange([1,2,5], 11))
+
+
+/**
+72. 编辑距离
+
+给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+
+你可以对一个单词进行如下三种操作：
+
+插入一个字符
+删除一个字符
+替换一个字符
+*/
+function minDistance(word1: string, word2: string): number {
+    const dp = new Array();
+    if (!dp[0]) dp[0] = [];
+    for (let i = 1; i <= word1.length; i++) {
+        if (!dp[i]) dp[i] = [];
+        dp[i][0] = i;
+    }
+    for (let j = 1; j <= word2.length; j++) {
+        dp[0][j] = j;
+    }
+    dp[0][0] = 0;
+
+    for (let i = 1; i <= word1.length; i++) {
+        for (let j = 1; j <= word2.length; j++) {
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1]
+            } else {
+                dp[i][j] = Math.min(
+                    dp[i - 1][j] + 1,
+                    dp[i][j - 1] + 1,
+                    dp[i - 1][j - 1] + 1
+                )
+            }
+        }
+    }
+    return dp[word1.length][word2.length];
+};
+
+console.log(minDistance("horse", "ros"))
