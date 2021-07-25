e98a92970aedafbd928877ad609a21b0e8eed14e
STDSuperman <2750556766@qq.com>
2021-07-25 13:02:24 +0800

    feat: 添加剑指offer 31 题

--- src/leetcode/ts-implement/回溯.ts
+++ src/leetcode/ts-implement/回溯.ts
@@ -73,4 +73,26 @@ function isValid(row: number, col: number, board: string[][]) {
     return result;
 };
 
-console.log(permute([1,2,3]))
+// console.log(permute([1,2,3]))
+
+
+/**
+ 剑指 Offer 31. 栈的压入、弹出序列
+ 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。假设压入栈的所有数字均不相等。例如，序列 {1,2,3,4,5} 是某栈的压栈序列，序列 {4,5,3,2,1} 是该压栈序列对应的一个弹出序列，但 {4,3,5,1,2} 就不可能是该压栈序列的弹出序列。
+ */
+
+
+function validateStackSequences(pushed: number[], popped: number[]): boolean {
+    const stack = [];
+    let j = 0;
+    for (let i = 0; i < pushed.length; i++) {
+        stack.push(pushed[i]);
+        while (stack.length && stack[stack.length - 1] === popped[j]) {
+            stack.pop();
+            j++;
+        }
+    }
+    return stack.length === 0;
+};
+
+console.log(validateStackSequences([2, 1, 0], [1, 2, 0]))
