487869f15ebb346bcb211a8b1ce72fb2ba5dbc9c
STDSuperman <2750556766@qq.com>
2021-09-16 10:07:49 +0800

    :wrench: feat: 添加combineReducers函数

--- src/react/redux-source-code-implement/core/index.ts
+++ src/react/redux-source-code-implement/core/index.ts
@@ -2,23 +2,24 @@
 export type ISubListenerCb = () => unknown;
 
 export type IAction = {
-	type: String;
+	type: String | Symbol;
 	[key: string]: any
 }
 
-export type IState = Record<string, any>
+export type IState = Record<string, any> | undefined
 
 export type IReducer = (state: any, action: IAction) => IState;
 
 class Store {
-	private state!: IState;
+	private state: IState;
 	private listeners: ISubListenerCb[] = [];
 	private reducer: IReducer;
 
-	constructor(reducer: IReducer) {
+	constructor(reducer: IReducer, initialState?: IState) {
+		this.state = initialState;
 		this.reducer = reducer;
 		this.dispatch({
-			type: `@@redux/__INIT__${Math.random()}`
+			type: Symbol(`@@redux/__INIT__${Math.random()}`)
 		})
 	}
 
@@ -42,11 +43,21 @@ class Store {
 	}
 };
 
-export const createStore = (reducer: IReducer) => {
-	const store = new Store(reducer);
+export const createStore = (reducer: IReducer, initialState?: IState) => {
+	const store = new Store(reducer, initialState);
 	return store;
 }
 
-export const combineReducers = () => {
-
+export const combineReducers = (reducerObj: Record<string, IReducer>): IReducer => {
+	const rdKeys = Object.keys(reducerObj);
+	return (state = {}, action): IState => {
+		const newState: IState = {};
+		for (let i = 0; i < rdKeys.length; i++) {
+			const key = rdKeys[i];
+			const reducer = reducerObj[key];
+			const prevState = state[key];
+			newState[key] = reducer(prevState, action);
+		}
+		return newState;
+	}
 }
--- src/react/redux-source-code-implement/sample/index.ts
+++ src/react/redux-source-code-implement/sample/index.ts
@@ -1,10 +1,15 @@
-import { createStore, IState, IAction } from '../core/index';
+import { createStore, IState, IAction, combineReducers } from '../core/index';
 
 const initialState = {
-	count: 0
+	reducer1: {
+		count: 0
+	},
+	reducer2: {
+		count: 0
+	}
 }
 
-function reducer(state: IState = initialState, action: IAction): IState {
+function reducer(state: IState = initialState.reducer1, action: IAction): IState {
 	switch (action.type) {
 		case 'INCREMENT':
 			return {...state, count: state.count + 1}
@@ -14,7 +19,20 @@ function reducer(state: IState = initialState, action: IAction): IState {
 	}
 }
 
-const store = createStore(reducer);
+function reducer1(state: IState = initialState.reducer2, action: IAction): IState {
+	switch (action.type) {
+		case 'INCREMENT':
+			return {...state, count: state.count + 1}
+		case 'DECREMENT':
+			return {...state, count: state.count - 1}
+		default: return state;
+	}
+}
+
+const store = createStore(combineReducers({
+	reducer1: reducer,
+	reducer2: reducer1
+}));
 
 store.subscribe(() => console.log(store.getState()))
 
